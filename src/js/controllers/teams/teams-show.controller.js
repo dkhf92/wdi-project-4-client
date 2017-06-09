06038fc227b32ac080dc50c2a82e5c2decd4ca26
dkhf92 <didrik@forsberg.to>
2017-06-09 12:10:15 +0100

    :fire:

--- src/js/controllers/teams/teams-show.controller.js
+++ src/js/controllers/teams/teams-show.controller.js
@@ -34,6 +34,39 @@ function TeamsShowCtrl(Team, $state, $stateParams){
     });
   };
 
+  // requestedTeams();
+  // function requestedTeams() {
+  //   Team
+  //   .query()
+  //   .$promise
+  //   .then(() => {
+  //     // vm.team = team;
+  //     const requested = [];
+  //     team.forEach(team => {
+  //       if(team.requestedBy.find(something => something.user._id === vm.user._id)) {
+  //         requested.push(team);
+  //       }
+  //     });
+  //     vm.requested = requested;
+  //   });
+  // }
+  // 
+  // vm.accept = function(request) {
+  //   Team.accept({ team_id: request.team_id, id: request.id })
+  //   .$promise
+  //   .then(data => {
+  //     console.log(data);
+  //     getTeam();
+  //     request.forEach(team => {
+  //       if(team.requestedBy.find(user => user.user._id === vm.user._id )) {
+  //         request.push(team);
+  //         console.log(request);
+  //       }
+  //     });
+  //     vm.requested = request;
+  //   });
+  // };
+
   vm.reject = function(request) {
     Team.reject({ team_id: request.team_id, id: request.id })
     .$promise
@@ -54,34 +87,34 @@ function TeamsShowCtrl(Team, $state, $stateParams){
   }
 
   // *****************************
-//   requestedTeams();
-//   function requestedTeams() {
-//     Team
-//     .query()
-//     .$promise
-//     .then(tasks => {
-//       const requested = [];
-//       tasks.forEach(team => {
-//         if(team.requestedBy.find(creator => creator.user._id === vm.user._id)) {
-//           requested.push(team);
-//         }
-//       });
-//       vm.requested = requested;
-//     });
-//   }
-//   assignedTeams();
-//   function assignedTeams() {
-//     Team
-//     .query()
-//     .$promise
-//     .then(teams => {
-//       const assigned = [];
-//       teams.forEach(team => {
-//         if(team.assignedTo.find(creator => creator === vm.user._id)) {
-//           assigned.push(team);
-//         }
-//       });
-//       vm.assigned = assigned;
-//     });
-//   }
+  //   requestedTeams();
+  //   function requestedTeams() {
+  //     Team
+  //     .query()
+  //     .$promise
+  //     .then(tasks => {
+  //       const requested = [];
+  //       tasks.forEach(team => {
+  //         if(team.requestedBy.find(creator => creator.user._id === vm.user._id)) {
+  //           requested.push(team);
+  //         }
+  //       });
+  //       vm.requested = requested;
+  //     });
+  //   }
+  //   assignedTeams();
+  //   function assignedTeams() {
+  //     Team
+  //     .query()
+  //     .$promise
+  //     .then(teams => {
+  //       const assigned = [];
+  //       teams.forEach(team => {
+  //         if(team.assignedTo.find(creator => creator === vm.user._id)) {
+  //           assigned.push(team);
+  //         }
+  //       });
+  //       vm.assigned = assigned;
+  //     });
+  //   }
 }
